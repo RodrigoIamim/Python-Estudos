552dc74c47ab83ad5094dd760d207e9a2f94340b
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-03-07 14:01:26 -0300

    up

--- Desafios/D42.py
+++ Desafios/D42.py
@@ -1,12 +1,13 @@
 a = float(input('Lado a: '))
 b = float(input('Lado b: '))
 c = float(input('Lado c: '))
-if a < b+c and b < a + c and c < b + c:
-    if a == b and a == c:
-        print(f'lados {a} {b} {c} formam triângulo EQUILÁTERO')
-    elif a == b or b == c and a != c:
-        print(f'lado {a} {b} {c} formam triângulo ISÓSCELES')
-    elif a != b and a!=c and b != c:
-        print(f'lados {a} {b} {c} formam triângulo ESCALENO')
+if a < b + c and b < a + c and c < a + b:
+    print('Podem formar um triângulo ', end='')
+    if a == b == c:
+        print(f'EQUILÁTERO')    
+    elif a != b != c != a:
+        print(f'ESCALENO')
+    else:
+        print(f'ISÓSCELES')
 else:
     print('NÃO É POSSÍVEL FORMAR UM TRIÂNGULO COM ESSAS MEDIDAS')
