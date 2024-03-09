a0b5a33195c7be66d46925a7f7126d8f3c29a66d
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-03-09 11:05:02 -0300

    upload

--- Desafios/D45.py
+++ Desafios/D45.py
@@ -4,7 +4,43 @@ print(f'{' JOKENPO ':=^40}')
 print('''SUAS OPÇÕES: 
 [0] PEDRA
 [1] PAPEL
-[3] TESOURA''')
-opc = int(input('FAÇA A SUA JOAGADA: '))
-if opc == 0:
+[2] TESOURA''')
+itens = ('PEDRA', 'PAPEL', 'TESOURA')
+computador = randint(0,2)
+jogador = int(input('FAÇA A SUA JOGADA: '))
+if jogador < 0 or jogador > 2:
+    print('OPÇÃO INVÁLIDA') 
+else: 
+    print('JO')
+    sleep(0.5)
+    print('KEN')
+    sleep(0.5)
+    print('PO')
+    sleep(0.2)
     
+    print('~' * 20)
+    print(f'COMPUTADOR: {itens[computador]}')
+    print(f'JOGADOR: {itens[jogador]}')
+    
+    if jogador == 0:
+        if computador == 0:
+            print('EMPATE!')
+        elif computador == 1:
+            print('JOGADOR VENCEU!')
+        elif computador == 2:
+            print('COMPUTADOR VENCEU!')
+    elif jogador == 1: 
+        if computador == 0:
+            print('JOGADOR VENCEU!')
+        elif computador == 1:
+            print('EMPATE!') 
+        elif computador == 2:
+            print('COMPUTADOR VENCEU!')
+    elif jogador == 2:
+        if computador == 0:
+            print('COMPUTADOR VENCEU!')
+        elif computador == 1:
+            print('JOGADOR VENCEU!')
+        elif computador == 2:
+            print('EMPATE!')
+    print('~' * 20)
