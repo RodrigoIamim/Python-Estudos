73c557e04b2212e1e5358e911950e95b88f8428f
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-03-14 19:25:55 -0300

    update

--- Desafios/D48.py
+++ Desafios/D48.py
@@ -1,6 +1,8 @@
 soma = 0
-for c in range(1, 500):
+qtd = 0
+for c in range(1, 501):
     if c%2==1 and c%3==0:
         soma += c
-print(f'A soma é {soma}')        
+        qtd += 1
+print(f'A soma dos {qtd} números encontados é {soma}')        
 print('fim')
