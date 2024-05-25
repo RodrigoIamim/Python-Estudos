183df94b79a6682d4d257edce986ac80249f0e79
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-05-24 21:37:36 -0300

    Create D56.py

---
+++ Desafios/D56.py
@@ -0,0 +1,13 @@
+media = 0
+for c in range(1, 5):
+    print('-'*20)
+    print(f'{c}° PESSOA')
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ').tolower)
+
+    if c == 1:
+        media = 0
+        velho = nome
+    elif:
+        sexo == 'f'
