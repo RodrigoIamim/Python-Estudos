af991698827bacc008d08e401d66ac4416b880dd
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-06-13 18:44:03 -0300

    up

--- Desafios/D59.py
+++ Desafios/D59.py
@@ -1,7 +1,7 @@
-n1 = float(input('Primeiro valor: '))
-n2 = float(input('Segundo valor: '))
-opc = 0
-while opc != 5:
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opc = '0'
+while opc != '5':
     print("""
 [1] somar
 [2] multiplicar
@@ -9,16 +9,14 @@ while opc != 5:
 [4] inserir novos valores
 [5] sair
 """)
-    opc = int(input('Selecione uma das opções: '))
-    if opc > 5:
-        print('OPÇÃO INVÁLIDA!')
-    if opc == 1:
+    opc = str(input('---> Selecione uma das opções: '))
+    if opc == '1':
         res = n1 + n2
         print(f'Resultado: {n1} + {n2} = {res} ')
-    if opc == 2:
+    elif opc == '2':
         res = n1*n2
         print(f'Resultado: {n1} x {n2} = {res} ')
-    if opc == 3:
+    elif opc == '3':
         if n1 > n2:
             print(f'Resultado: {n1} é maior que {n2}  ')
         elif n2 > n1:
@@ -26,10 +24,13 @@ while opc != 5:
             print(f'Resultado: {n2} é maior que {n1}')
         else:
             print('ambos possuem o mesmo valor!')
-    if opc == 4:
-        n1 = float(input('Novo primeiro valor: '))
-        n2 = float(input('Novo segundo valor: '))
-    print(':-:'*10)
+    elif opc == '4':
+        n1 = int(input('Novo primeiro valor: '))
+        n2 = int(input('Novo segundo valor: '))
+    elif opc == '5':
+        print('Encerrando...')
+    else:
+        print('VALOR INVÁLIDO!')
 print('FIM! :)')
 
     
