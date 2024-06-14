cf6a648e6036fbb96b49cd22e0f1c5b8449281b9
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-06-13 22:22:19 -0300

    Create D60.py

---
+++ Desafios/D60.py
@@ -0,0 +1,12 @@
+'''from math import factorial
+print('FATORIAL 1')
+n1 = int(input('Insira um número inteiro: '))
+fat = factorial(n1)
+print(f'O fatorial de {n1} é {fat}')'''
+
+print('FATORIAL 2')
+n1 = int(input('Insira um número inteiro: '))
+c = n1
+while c != 0:
+    c-=1
+    fat = c
