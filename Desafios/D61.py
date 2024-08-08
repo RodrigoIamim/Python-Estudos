1a6dff653949e606f0615f43ed47c3af2a216c5f
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-08-08 17:29:58 -0300

    Update D61.py

--- Desafios/D61.py
+++ Desafios/D61.py
@@ -3,7 +3,7 @@ n1 = int(input('Primeiro termo: '))
 r = int(input('Razão: '))
 print(f'Calculando os 10 primeiros termos da PA partindo do número {n1} com razão {r}... ')
 c = 1
-pt = n1
+pt = n1 
 sleep(1)
 while c < 11:
     c+=1
