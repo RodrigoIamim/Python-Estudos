5bad8aa0ad00e3b5efc6b600e702d91e3a9469a3
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-08-19 18:40:47 -0300

    finalizando ex65

--- Desafios/D65.py
+++ Desafios/D65.py
@@ -1,7 +1,19 @@
 print('Maior e menor valores')
 resp = 'S'
-maior = menor = 0
+maior = menor = media = soma = totn = 0
 while resp in 'Ss':
     n = int(input('Digite um valor: '))
+    totn += 1
+    soma += n
+    if totn == 1:
+        maior = menor = n
+    elif totn > 1:
+        if n > maior:
+            maior = n
+        elif n < menor:
+            menor = n
     resp = input('Quer continuar [S/N] ? ').upper().strip()[0]
+media = soma/totn
+print(f'A média entre TODOS os {totn} valores é {media:.2f}')
+print(f'{maior} foi o maior e {menor} foi o menor')
 print('fim')
