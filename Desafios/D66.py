3d7f89206ecfeb009b12b217bbd8edbe0f4e8aa7
Rodrigo Ramos Iamim <85762822+RodrigoIamim@users.noreply.github.com>
2024-09-25 20:01:51 -0300

    Update D66.py

--- Desafios/D66.py
+++ Desafios/D66.py
@@ -1,7 +1,8 @@
-n = s = 0
+n = s = t = 0
 while True:
     n = int(input('Digite um numero: '))
     if n == 999:
         break
+    t+=1
     s+=n
-print(f'a soma entre eles é {s}')
+print(f'a soma entre os {t} números digitados é {s}')
