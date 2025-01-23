2364b1b6d0798ba578849b394b40fc7e5f9be7d1
Rodrigo Ramos Iamim <iamim.rodrigo.42@gmail.com>
2025-01-23 14:36:35 -0300

    Update D70.py

--- Desafios/D70.py
+++ Desafios/D70.py
@@ -22,6 +22,9 @@ while True:
         resp = str(input('Continuar comprando? [S/N] ')).upper().strip()[0]
     if resp == 'N':
         break    
+print('-=-'*10, end = '')
+print(' RESULTADO ', end=' ')
+print('-=-'*10)
 print(f'minimo {min}')
 print(f'maximo {max}')
 print(f'acima de mil {totmil}')
