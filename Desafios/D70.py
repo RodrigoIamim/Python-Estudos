e7c3ad0ef8a15e0b5d1457f8a3fbfc28a51bd2b9
Rodrigo Ramos Iamim <iamim.rodrigo.42@gmail.com>
2025-01-23 21:49:23 -0300

    lalala

--- Desafios/D70.py
+++ Desafios/D70.py
@@ -5,17 +5,19 @@ total = min = totmil = max = 0
 c = 1
 while True:
     produto = str(input('Produto: ')).strip().upper()
-    valor = float(input('Valor: '))
+    valor = float(input('Valor: R$'))
     total += valor
     if valor > 1000:
         totmil += 1
     if c == 1:
         min = max = valor
+        maisbarato = produto
     else:
         if valor > max:
             max = valor
         if valor < min:
             min = valor
+            maisbarato = produto
     c += 1
     resp = ' '
     while resp not in 'SN':
@@ -25,7 +27,7 @@ while True:
 print('-=-'*10, end = '')
 print(' RESULTADO ', end=' ')
 print('-=-'*10)
-print(f'minimo {min}')
-print(f'maximo {max}')
-print(f'acima de mil {totmil}')
-print(f'total {total}')
+print(f'Mínimo: R${min}, {maisbarato}')
+print(f'Máximo: R${max}')
+print(f'Acima de mil: {totmil}')
+print(f'Total: R${total}')
---
+++ Desafios/D71.py
@@ -0,0 +1,3 @@
+print('-=-'*10, end=' ')
+print(' CEVBANK ', end=' ')
+print('-=-'*10)
