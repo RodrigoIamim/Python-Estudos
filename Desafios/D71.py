41056c3b56bb54bfa1088608cf872dac76d36917
Rodrigo Ramos Iamim <iamim.rodrigo.42@gmail.com>
2025-01-28 18:00:41 -0300

    Update D71.py

--- Desafios/D71.py
+++ Desafios/D71.py
@@ -2,10 +2,24 @@ print('-=-'*10, end=' ')
 print(' CEVBANK ', end=' ')
 print('-=-'*10)
 valor = int(input('Digite o valor a ser sacado (SOMENTE OS NÚMEROS): R$'))
-print(f'{valor} em notas de 50, 20, 10 e 1')
+total = valor
+cedula = 50
+totced = 0
 while True:
-    c50 = valor//50
-    valor-=50
-    if valor//50==0:
-        break
-print(f'{c50} notas de 50 para {valor}')
+    if total >= cedula:
+        total -= cedula
+        totced += 1
+        
+    else:
+        if totced > 0:
+            print(f'Total de cédulas de {cedula}: {totced}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totced = 0
+        if total == 0:
+            break
+print('fim')        
